af2204f04c332cf464118a97d57577b0c8e3cea6
Nguyễn Văn Bảo Duy <117926234+GGevolt@users.noreply.github.com>
2024-02-22 06:16:45 +0700

    Add project files.

---
+++ StudentContributions.DataAccess/Data/ApplicationDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentContributions.DataAccess
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> option) : base(option)
+        {
+
+        }
+    }
+}
