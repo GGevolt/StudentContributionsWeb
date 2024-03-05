02df07554757f96a542b11a68f723518a2f4aea5
GWMinhTHD <minhthdgcd210662@fpt.edu.vn>
2024-03-05 14:46:48 +0700

    Enable HTML for mail

--- StudentContributions/Services/EmailSenderService.cs
+++ StudentContributions/Services/EmailSenderService.cs
@@ -9,6 +9,7 @@ namespace StudentContributions.Services
         {
             SmtpClient smtpClient = new SmtpClient("127.0.0.1", 25);
             MailMessage mail = new MailMessage("system@bulul.cunny", email, subject, htmlMessage);
+            mail.IsBodyHtml = true;
 
             return smtpClient.SendMailAsync(mail);
         }
