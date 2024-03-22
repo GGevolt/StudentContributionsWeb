9219ce62f71ead49cd913e958d6a1325877365f6
GWMinhTHD <minhthdgcd210662@fpt.edu.vn>
2024-03-22 22:09:34 +0700

    Add file upload and download for an article/contribution

--- StudentContributions/Areas/Student/Controllers/ContributionController.cs
+++ StudentContributions/Areas/Student/Controllers/ContributionController.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using StudentContributions.DataAccess.Repository.IRepository;
 using StudentContributions.Models.Models;
+using StudentContributions.Models.ViewModels;
+using System.IO;
+using System.IO.Compression;
 
 namespace StudentContributions.Areas.Student.Controllers
 {
@@ -10,10 +14,12 @@ namespace StudentContributions.Areas.Student.Controllers
     public class ContributionController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
+        private IWebHostEnvironment _webHost;
 
-        public ContributionController(IUnitOfWork unitOfWork)
+        public ContributionController(IUnitOfWork unitOfWork, IWebHostEnvironment webHost)
         {
             _unitOfWork = unitOfWork;
+            _webHost = webHost;
         }
 
         public IActionResult Index()
@@ -29,15 +35,113 @@ namespace StudentContributions.Areas.Student.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(Contribution contribution)
+        public IActionResult Create(Contribution contribution, List<IFormFile>? files)
         {
             if (ModelState.IsValid)
             {
                 _unitOfWork.ContributionRepository.Add(contribution);
                 _unitOfWork.Save();
+
+                string uploadPath = Path.Combine(this._webHost.WebRootPath, "Contributions", contribution.ID.ToString());
+                if (!Directory.Exists(uploadPath)) Directory.CreateDirectory(uploadPath);
+
+                foreach (var file in files)
+                {
+                    string fileName = Path.GetFileNameWithoutExtension(file.FileName) + "_" + Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    using (var fileStream = new FileStream(Path.Combine(uploadPath, fileName), FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+                }
                 return RedirectToAction(nameof(Index));
             }
             return View(contribution);
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var contribution = _unitOfWork.ContributionRepository.Get(c => c.ID == id);
+            if (contribution == null)
+            {
+                return NotFound();
+            }
+            ConDetails details = new ConDetails();
+            details.Filenames = new List<string>();
+            details.Contribution = contribution;
+            string path = Path.Combine(this._webHost.WebRootPath, "Contributions", contribution.ID.ToString());
+            if (Directory.Exists(path))
+            {
+                string[] paths = Directory.GetFiles(Path.Combine(_webHost.WebRootPath, "Contributions", contribution.ID.ToString()));
+                foreach (string file in paths)
+                {
+                    details.Filenames.Add(Path.GetFileName(file));
+                }
+            }
+            return View(details);
+        }
+
+        [HttpPost]
+        public IActionResult Details(List<IFormFile>? files, int id)
+        {
+            string uploadPath = Path.Combine(_webHost.WebRootPath, "Contributions", id.ToString());
+            if (!Directory.Exists(uploadPath)) Directory.CreateDirectory(uploadPath);
+
+            foreach (var file in files)
+            {
+                string fileName = Path.GetFileNameWithoutExtension(file.FileName) + "_" + Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                using (var fileStream = new FileStream(Path.Combine(uploadPath, fileName), FileMode.Create))
+                {
+                    file.CopyTo(fileStream);
+                }
+            }
+            return RedirectToAction("Details", new { id = id });
+        }
+
+        public FileResult DownloadFile(string fileName, int id)
+        {
+            string path = Path.Combine(_webHost.WebRootPath, "Contributions", id.ToString()+"/") + fileName;
+
+            byte[] bytes = System.IO.File.ReadAllBytes(path);
+
+            return File(bytes, "application/octet-stream", fileName);
+        }
+
+        public IActionResult DeleteFile(string fileName, int id)
+        {
+            string path = Path.Combine(_webHost.WebRootPath, "Contributions", id.ToString() + "/") + fileName;
+
+            FileInfo file = new FileInfo(path);
+            if (file.Exists)
+            {
+                file.Delete();
+            }
+
+            return RedirectToAction("Details", new { id =  id});
+        }
+
+        public FileResult DownloadZip(int id)
+        {
+            string[] paths = Directory.GetFiles(Path.Combine(_webHost.WebRootPath, "Contributions", id.ToString()));
+
+            MemoryStream memoryStream = new MemoryStream();
+
+            using (var zipArchive = new ZipArchive(memoryStream, ZipArchiveMode.Create, true))
+            {
+                foreach (var file in paths)
+                {
+                    zipArchive.CreateEntryFromFile(file, Path.GetFileName(file));
+                }
+            }
+
+            memoryStream.Position = 0;
+
+            var con = _unitOfWork.ContributionRepository.Get(c => c.ID == id);
+            string filename = "Article_"+id.ToString()+"_"+ con.Title+".zip";
+            return File(memoryStream, "application/zip", filename);
         }
 
         public IActionResult Edit(int? id)
