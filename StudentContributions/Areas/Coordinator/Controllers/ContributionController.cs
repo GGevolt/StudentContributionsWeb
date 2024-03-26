20ed3e47db32c271fdcb11a684c1ae69365be32f
GWMinhTHD <minhthdgcd210662@fpt.edu.vn>
2024-03-26 11:16:51 +0700

    view all articles of a magazine

---
+++ StudentContributions/Areas/Coordinator/Controllers/ContributionController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentContributions.Areas.Coordinator.Controllers
+{
+    public class ContributionController : Controller
+    {
+        public IActionResult Index()
+        {
+            //edit contribtion
+            return View();
+        }
+    }
+}
